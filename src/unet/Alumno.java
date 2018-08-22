ba5f4b422b76c5c30f69ba0a6a1cd3cdde85fc8f
john llanes <jhonharleyllanesescobar@hotmail.com>
2018-08-22 18:46:32 -0400

    Agregando a profesor

--- src/unet/Alumno.java
+++ src/unet/Alumno.java
@@ -5,6 +5,7 @@
  */
 package unet;
 import unet.Persona;
+import java.io.*;
 /**
  *
  * @author CASA
@@ -17,11 +18,30 @@ public class Alumno extends Persona {
         super();
         this.matricula=0;this.creditos=0;this.carrera=" ";
     }
-    public Alumno(String n,String c,String t,int e,int matricula,int creditos,String carrera){
+    public Alumno(String n,String c,String t,int e,int matricula,int creditos,
+            String carrera){
         super(n,c,t,e);
         this.matricula=matricula;
         this.creditos=creditos;
         this.carrera=carrera;
+    }
+    public void cargar_datos() throws IOException {
+        DataInputStream lector=new DataInputStream(System.in);
+        System.out.println("Registro de Alumndo");
+        super.cargar_Dato_DataInputStream();
+        System.out.println("Numero de la matricula del alumno");
+        this.matricula=Integer.parseInt(lector.readLine());
+        System.out.println("Carrera");
+        this.carrera=lector.readLine();
+        System.out.println("Creditos");
+        this.creditos=Integer.parseInt(lector.readLine());
+    }
+    public void mostar_Datos(){
+        System.out.println("Datos de Alumno:");
+        super.mostrar_Datos();
+        System.out.println("Matricula: "+this.matricula);
+        System.out.println("Carrera: "+this.carrera);
+        System.out.println("Creditos: "+this.creditos);
     }
 
     public int getMatricula() {
